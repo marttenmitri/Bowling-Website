d97f494a52033a61cce07a231cd863f14fa640d2
Martten Mitri <55189803+marttenmitri@users.noreply.github.com>
2024-08-01 16:47:20 +0300

    Added updated fields, fixed gallery

--- gallery.js
+++ gallery.js
@@ -1,3 +1,4 @@
+// Houses
 const mainHouseImages = [
     "./img/out-1.png",
     "./img/room-1.png",
@@ -6,26 +7,70 @@ const mainHouseImages = [
 
 const greyHouseImages = [
     "./img/out-large-2.png",
-    // Add other image paths here
 ];
 
 const abHouseImages = [
     "./img/out-large-3.png",
-    // Add other image paths here
 ];
 
 const smallHouseImages = [
     "./img/out-large-4.png",
-    // Add other image paths here
 ];
 
+const campHouseImages = [
+    "./img/sauna_1.jpg",
+];
+
+const guestHouseImages = [
+    "./img/sauna_2.jpg",
+];
+
+// Saunas
 const sauna_1 = [
     "./img/sauna_1.jpg",
 ];
 
 const sauna_2 = [
     "./img/sauna_2.jpg",
+];
 
+const sauna_3 = [
+    "./img/sauna_2.jpg",
+    "./img/sauna_1.jpg",
+];
+
+// Halls
+const smallHallImages = [
+    "./img/sauna_1.jpg",
+];
+
+const fireplaceHallImages = [
+    "./img/sauna_2.jpg",
+];
+
+const cinemaHallImages = [
+    "./img/sauna_2.jpg",
+];
+
+const mainHallImages = [
+    "./img/sauna_2.jpg",
+];
+
+// Entertainment
+const tennisImages = [
+    "./img/sauna_2.jpg",
+];
+
+const grillImages = [
+    "./img/sauna_2.jpg",
+];
+
+const cafeImages = [
+    "./img/sauna_2.jpg",
+];
+
+const beachCafeImages = [
+    "./img/sauna_2.jpg",
 ];
 
 const houseImages = {
@@ -35,6 +80,18 @@ const houseImages = {
     smallHouse: smallHouseImages,
     sauna_1: sauna_1,
     sauna_2: sauna_2,
+    sauna_3: sauna_3,
+    campHouse: campHouseImages,
+    guestHouse: guestHouseImages,
+    smallHall: smallHallImages,
+    fireplaceHall: fireplaceHallImages,
+    cinemaHall: cinemaHallImages,
+    mainHall: mainHallImages,
+    tennis: tennisImages,
+    grill: grillImages,
+    cafe: cafeImages,
+    beachCafe: beachCafeImages,
+
 };
 
 function openModal(modalId) {
@@ -43,22 +100,21 @@ function openModal(modalId) {
     slidesContainer.innerHTML = ''; // Clear existing slides
 
     const images = houseImages[modalId];
-    console.log("images")
-    console.log(images)
     images.forEach((src, index) => {
         const slide = document.createElement('div');
         slide.className = 'mySlides';
-        slide.innerHTML = `<img src="${src}" style="width:100%">`;
+        slide.innerHTML = `<img src="${src}" style="width:auto; max-width:100%; max-height:90vh;">`; // Ensure img element adapts to modal size
         slidesContainer.appendChild(slide);
     });
 
-    modal.style.display = 'block';
-    document.querySelector('.navbar').style.zIndex = 1;
+    modal.style.display = 'flex'; // Display modal as flex to center content
+    document.querySelector('.navbar').style.zIndex = 13;
     document.body.style.overflow = 'hidden';
 
     // Show the first slide
     showSlides(1, modalId);
 }
+
 
 function closeModal(modalId) {
     document.getElementById(modalId).style.display = 'none';
@@ -73,6 +129,17 @@ var slideIndices = {
     smallHouse: 1,
     sauna_1: 1,
     sauna_2: 1,
+    sauna_3: 1,
+    campHouse: 1,
+    guestHouse: 1,
+    smallHall: 1,
+    fireplaceHall: 1,
+    cinemaHall: 1,
+    mainHall: 1,
+    tennis: 1,
+    grill: 1,
+    cafe: 1,
+    beachCafe: 1,
 };
 
 function plusSlides(n, modalId) {
